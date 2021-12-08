f85d773ecdfa9817b9999bcf5a09d83a365442e9
SarahIT200 <Sarah.Ali.abt.js@tuwaiq.edu.sa>
2021-12-08 20:00:21 +0300

    commit

--- src/pages/OneFilm.js
+++ src/pages/OneFilm.js
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { Card } from "react-bootstrap"
+import { Card, Col, Row } from "react-bootstrap"
 import { useParams } from "react-router"
 import { Link } from "react-router-dom"
 import FilmsContext from "../utils/FilmsContext"
@@ -12,17 +12,35 @@ function OneFilm() {
   const film = films.find(film => film._id === filmId)
   return (
     <>
-      <Card border="light">
-        <Link to={`/film/${film._id}`}>
-          <Card.Img variant="top" src={film.poster} height="220px" style={{ borderRadius: "10px" }} />
-        </Link>
-        <Card.Body>
-          <Link to={`/film/${film._id}`} className="text-black" style={{ textDecoration: "none" }}>
-            <Card.Title>{film.title}</Card.Title>
-          </Link>
-          <Card.Text className="text-muted">{film.description}</Card.Text>
-        </Card.Body>
+      <Card className="mt-5">
+        <Row className="d-flex align-items-center justify-content-center">
+          <Col>
+            <Card.Img variant="top" className="m-3" src={film.poster} height="500px" style={{ borderRadius: "10px" }} />
+          </Col>
+          <Col className="ms-5">
+            <Row>
+              <Card.Title>{film.title}</Card.Title>
+            </Row>
+            <Row>
+              <Card.Text>{film.ratingAverage}</Card.Text>
+              <Card.Text className="text-muted">{film.description}</Card.Text>
+            </Row>
+          </Col>
+        </Row>
       </Card>
+      <h5 className="m-3"> top build cast</h5>
+      <Row className="m-3">
+        {film.actors.map(actor => (
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={actor.photo} height="200px" width="100px" style={{ borderRadius: "10px" }} />
+              <Card.Title className="ms-4">
+                {actor.firstName} {actor.lastName}
+              </Card.Title>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </>
   )
 }
