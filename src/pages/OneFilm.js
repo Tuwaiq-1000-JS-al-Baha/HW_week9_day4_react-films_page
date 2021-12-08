4bda1ac2db004c44283278fa6be982995d276b36
SuaadAlgamdi <Suaad.Abduallh.abt.js@tuwaiq.edu.sa>
2021-12-08 19:04:19 +0300

    this is my Home work

--- src/pages/OneFilm.js
+++ src/pages/OneFilm.js
@@ -1,5 +1,6 @@
+import { CardHeader } from "@mui/material"
 import { useContext } from "react"
-import { Card } from "react-bootstrap"
+import { Card, Container, Row, Col, Image } from "react-bootstrap"
 import { useParams } from "react-router"
 import { Link } from "react-router-dom"
 import FilmsContext from "../utils/FilmsContext"
@@ -23,6 +24,38 @@ function OneFilm() {
           <Card.Text className="text-muted">{film.description}</Card.Text>
         </Card.Body>
       </Card>
+      <Container className="mt-5 md-3">
+        <Row className="">
+          <Col md="6">
+            <h3> Owner</h3>
+          </Col>
+          <Col md="6">
+            <Image src={film._user.poster} style={{ heigt: 200 }} />
+          </Col>
+          <Col md={4}>
+            <h3>
+              {film._user.firstName} {film._user.lastName}
+            </h3>
+          </Col>
+        </Row>
+
+        <Row className="mt-5 d=flex align-items-center">
+          <Col>
+            <Image src={film.poster} style={{ height: 400 }} thumbnail />
+          </Col>
+
+          <Col>
+            <Card>
+              <CardHeader>
+                <Card.Title className="text-center">{film.title} </Card.Title>
+              </CardHeader>
+              <Card.Body style={{ height: 200 }}>
+                <Card.Text className="text-center">{film.description} </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
