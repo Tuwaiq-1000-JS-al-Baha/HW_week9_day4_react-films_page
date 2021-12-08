270cf64e1231e6d57dfce99b6e8d7bad40caf9e5
SarahIT200 <Sarah.Ali.abt.js@tuwaiq.edu.sa>
2021-12-08 21:56:28 +0300

    commit

--- src/pages/OneFilm.js
+++ src/pages/OneFilm.js
@@ -13,21 +13,42 @@ function OneFilm() {
   return (
     <>
       <Card className="mt-5">
-        <Row className="d-flex align-items-center justify-content-center">
-          <Col>
-            <Card.Img variant="top" className="m-3" src={film.poster} height="500px" style={{ borderRadius: "10px" }} />
-          </Col>
-          <Col className="ms-5">
-            <Row>
-              <Card.Title>{film.title}</Card.Title>
-            </Row>
-            <Row>
-              <Card.Text>{film.ratingAverage}</Card.Text>
-              <Card.Text className="text-muted">{film.description}</Card.Text>
-            </Row>
-          </Col>
-        </Row>
+        <Col
+          style={{
+            backgroundImage: `url("${film.poster}" )`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            // height: 400,
+            // display: "flex",
+            // flexDirection: "column",
+            // justifyContent: "center",
+            opacity: "0.8",
+          }}
+        >
+          <Row className="d-flex align-items-center justify-content-center">
+            <Col>
+              <Card.Img
+                variant="top"
+                className="m-3"
+                src={film.poster}
+                height="500px"
+                style={{ borderRadius: "10px" }}
+              />
+            </Col>
+            <Col className="ms-5">
+              <Row>
+                <Card.Title style={{ color: "white" }}>{film.title}</Card.Title>
+              </Row>
+              <Row>
+                <Card.Text style={{ color: "white" }}>{film.ratingAverage}</Card.Text>
+                <Card.Text style={{ color: "white" }}>{film.description}</Card.Text>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
       </Card>
+
       <h5 className="m-3"> top build cast</h5>
       <Row className="m-3">
         {film.actors.map(actor => (
