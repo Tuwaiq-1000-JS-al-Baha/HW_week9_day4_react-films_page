17bb0f536b92550e09af8b04de464db98c542b20
X702-2025 <mohmmad201048@gmail.com>
2021-12-08 22:07:55 +0300

    done

--- src/pages/OneFilm.js
+++ src/pages/OneFilm.js
@@ -1,9 +1,9 @@
+import { display } from "@mui/system"
 import { useContext } from "react"
-import { Card } from "react-bootstrap"
+import { Card, Col, Container, Row } from "react-bootstrap"
 import { useParams } from "react-router"
-import { Link } from "react-router-dom"
 import FilmsContext from "../utils/FilmsContext"
-
+import "./OneFilm.css"
 function OneFilm() {
   const { filmId } = useParams()
   const { films } = useContext(FilmsContext)
@@ -12,17 +12,51 @@ function OneFilm() {
   const film = films.find(film => film._id === filmId)
   return (
     <>
-      <Card border="light">
-        <Link to={`/film/${film._id}`}>
-          <Card.Img variant="top" src={film.poster} height="220px" style={{ borderRadius: "10px" }} />
-        </Link>
-        <Card.Body>
-          <Link to={`/film/${film._id}`} className="text-black" style={{ textDecoration: "none" }}>
-            <Card.Title>{film.title}</Card.Title>
-          </Link>
-          <Card.Text className="text-muted">{film.description}</Card.Text>
-        </Card.Body>
-      </Card>
+      <div className="content">
+        <div className="div">
+          <Card className="mt-5">
+            <Row className="d-flex align-items-center justify-content-center">
+              <Col>
+                <Card.Img
+                  variant="top"
+                  className="poster"
+                  src={film.poster}
+                  height="500px"
+                  style={{ borderRadius: "10px" }}
+                />
+              </Col>
+              <div className="text">
+                <Col className="ms-5" id="col">
+                  <Row>
+                    <Card.Title id="title">{film.title}</Card.Title>
+                  </Row>
+                  <Row>
+                    <Card.Text>{film.ratingAverage}/5</Card.Text>
+                    <Card.Text className="text-muted" id="desc">
+                      {film.description}
+                    </Card.Text>
+                  </Row>
+                </Col>
+              </div>
+            </Row>
+          </Card>
+        </div>
+        <div>
+          <h4 className="m-3"> Actors</h4>
+          <Row className="m-3">
+            {film.actors.map(actor => (
+              <Col>
+                <Card>
+                  <Card.Img className="poster" variant="top" src={actor.photo} style={{ borderRadius: "10px" }} />
+                  <Card.Title className="ms-4">
+                    {actor.firstName} {actor.lastName}
+                  </Card.Title>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+      </div>
     </>
   )
 }
