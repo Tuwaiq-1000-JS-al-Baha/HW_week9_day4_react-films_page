9c85ed84320b0b42caa6740d75930ea911f42615
Adnen Ben Amor <adnene.benamor@tuwaiq.edu.sa>
2021-12-08 17:07:01 +0300

    added homework

---
+++ src/pages/OneFilm.js
@@ -0,0 +1,30 @@
+import { useContext } from "react"
+import { Card } from "react-bootstrap"
+import { useParams } from "react-router"
+import { Link } from "react-router-dom"
+import FilmsContext from "../utils/FilmsContext"
+
+function OneFilm() {
+  const { filmId } = useParams()
+  const { films } = useContext(FilmsContext)
+  if (films.length === 0) return <h1>Loading...</h1>
+
+  const film = films.find(film => film._id === filmId)
+  return (
+    <>
+      <Card border="light">
+        <Link to={`/film/${film._id}`}>
+          <Card.Img variant="top" src={film.poster} height="220px" style={{ borderRadius: "10px" }} />
+        </Link>
+        <Card.Body>
+          <Link to={`/film/${film._id}`} className="text-black" style={{ textDecoration: "none" }}>
+            <Card.Title>{film.title}</Card.Title>
+          </Link>
+          <Card.Text className="text-muted">{film.description}</Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  )
+}
+
+export default OneFilm
