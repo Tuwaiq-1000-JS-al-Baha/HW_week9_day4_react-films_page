3099f681ef95a6f5661b532c99cba3e2f1d6ee1e
Mohammmed1101 <Mohammed.Khaled.abt.js@tuwaiq.edu.sa>
2021-12-08 23:09:50 +0300

    done HW

--- src/pages/OneFilm.js
+++ src/pages/OneFilm.js
@@ -1,8 +1,9 @@
 import { useContext } from "react"
-import { Card } from "react-bootstrap"
+import { Card, Row, Col, Container } from "react-bootstrap"
 import { useParams } from "react-router"
 import { Link } from "react-router-dom"
 import FilmsContext from "../utils/FilmsContext"
+// import Styles from"./moduleOneFilm.css"
 
 function OneFilm() {
   const { filmId } = useParams()
@@ -12,17 +13,47 @@ function OneFilm() {
   const film = films.find(film => film._id === filmId)
   return (
     <>
-      <Card border="light">
-        <Link to={`/film/${film._id}`}>
-          <Card.Img variant="top" src={film.poster} height="220px" style={{ borderRadius: "10px" }} />
-        </Link>
-        <Card.Body>
-          <Link to={`/film/${film._id}`} className="text-black" style={{ textDecoration: "none" }}>
-            <Card.Title>{film.title}</Card.Title>
-          </Link>
-          <Card.Text className="text-muted">{film.description}</Card.Text>
-        </Card.Body>
+    <Card border="light">
+      <Container>
+        <Row>
+  <div
+      className="bg-image d-flex justify-content-center align-items-center"
+      style={{
+        backgroundImage: `url("${film.poster}")`,
+        backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+           height:"80vh"
+          
+      }}
+      >
+
+
+     
+          
+
+          <Col>
+            <Link to={`/film/${film._id}`}>
+              <Card.Img variant="top" src={film.poster} height="400px" style={{ borderRadius: "10px" }} />
+            </Link>
+            </Col>
+
+              <Col>
+              <Card.Body>
+                <Link to={`/film/${film._id}`} className="text-white mt-5" style={{textAlign:"center",   textDecoration: "none", }}>
+                  <Card.Title>{film.title}</Card.Title>
+                </Link>
+                <Card.Text className="text-white mt-4" style={{textAlign:"center"}}>{film.description}</Card.Text>
+              </Card.Body>
+              </Col>
+             
+             
+              </div>
+        </Row>
+        </Container>
       </Card>
+
+
     </>
   )
 }
