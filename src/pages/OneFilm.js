6ee1fcca380c7f50d42b7ff723102730dbd8bd85
bader0007 <89877474+bader0007@users.noreply.github.com>
2021-12-09 02:46:14 +0300

    hw

--- src/pages/OneFilm.js
+++ src/pages/OneFilm.js
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { Card } from "react-bootstrap"
+import { Card, Col, Row } from "react-bootstrap"
 import { useParams } from "react-router"
 import { Link } from "react-router-dom"
 import FilmsContext from "../utils/FilmsContext"
@@ -23,6 +23,35 @@ function OneFilm() {
           <Card.Text className="text-muted">{film.description}</Card.Text>
         </Card.Body>
       </Card>
+      <Card className="mt-5">
+        <Row className="d-flex align-items-center justify-content-center">
+          <Col>
+            <Card.Img variant="top" className="m-3" src={film.poster} height="450px" style={{ borderRadius: "10px" }} />
+          </Col>
+          <Col className="ms-5">
+            <Row>
+              <Card.Title>{film.title}</Card.Title>
+            </Row>
+            <Row>                                  
+              <Card.Text>{film.ratingAverage}</Card.Text> 
+              <Card.Text className="text-muted">{film.description}</Card.Text>
+            </Row>     
+          </Col>
+        </Row>
+      </Card>
+      <h5 className="m-3"> </h5>
+      <Row className="m-3">
+        {film.actors.map(actor => (
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={actor.photo} height="250px" width="100px" style={{ borderRadius: "10px" }} />
+              <Card.Title className="ms-4">
+                {actor.firstName} {actor.lastName}
+              </Card.Title>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </>
   )
 }
