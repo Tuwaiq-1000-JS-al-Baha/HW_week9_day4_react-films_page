ab5d0f7ece8af813303df00269ace5e5676bf1ed
Abdulathim96 <Abdulathim.Abdullah.abt.js@tuwaiq.edu.sa>
2021-12-09 02:17:40 +0300

    home work

--- src/pages/OneFilm.js
+++ src/pages/OneFilm.js
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { Card } from "react-bootstrap"
+import { Card, Row, Col, Image } from "react-bootstrap"
 import { useParams } from "react-router"
 import { Link } from "react-router-dom"
 import FilmsContext from "../utils/FilmsContext"
@@ -12,17 +12,56 @@ function OneFilm() {
   const film = films.find(film => film._id === filmId)
   return (
     <>
-      <Card border="light">
-        <Link to={`/film/${film._id}`}>
-          <Card.Img variant="top" src={film.poster} height="220px" style={{ borderRadius: "10px" }} />
-        </Link>
-        <Card.Body>
-          <Link to={`/film/${film._id}`} className="text-black" style={{ textDecoration: "none" }}>
-            <Card.Title>{film.title}</Card.Title>
+      <div
+        style={{
+          objectFit: "contain",
+          opacity: "0.4",
+          backgroundImage: `url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1-IOUAej-IexBNvG4zUBJbNzJvIkPiwkkLw&usqp=CAU)`,
+          position: "absolute",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          width: "100vw",
+          height: "100vh",
+        }}
+      />
+      <Row className="m-5">
+        <Col>
+          <Link to={`/film/${film._id}`}>
+            <Image
+              variant="top"
+              src={film.poster}
+              height="500px"
+              style={{
+                opacity: "0.9",
+                width: "400px",
+                paddingTop: "20px",
+              }}
+            />
           </Link>
-          <Card.Text className="text-muted">{film.description}</Card.Text>
-        </Card.Body>
-      </Card>
+        </Col>
+        <Col>
+          <Card.Body>
+            <Link
+              to={`/film/${film._id}`}
+              style={{
+                textDecoration: "none",
+              }}
+            >
+              <Card.Title>{film.title}</Card.Title>
+            </Link>
+            <Card.Text
+              style={{
+                top: "60px",
+                right: "250px",
+                fontSize: "15px",
+              }}
+            >
+              {film.description}
+            </Card.Text>
+          </Card.Body>
+        </Col>
+      </Row>
     </>
   )
 }
