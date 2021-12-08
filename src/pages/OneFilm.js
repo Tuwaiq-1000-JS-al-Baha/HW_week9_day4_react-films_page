9387787e6850001d9c01dac78b5adacf8eab3bd8
Eyad5501 <Eyad.Ahmed.abt.js@tuwaiq.edu.sa>
2021-12-09 00:14:36 +0300

    Update OneFilm.js

--- src/pages/OneFilm.js
+++ src/pages/OneFilm.js
@@ -1,8 +1,8 @@
 import { useContext } from "react"
-import { Card } from "react-bootstrap"
 import { useParams } from "react-router"
 import { Link } from "react-router-dom"
 import FilmsContext from "../utils/FilmsContext"
+import { Card, Row, Col, Container } from "react-bootstrap"
 
 function OneFilm() {
   const { filmId } = useParams()
@@ -12,17 +12,43 @@ function OneFilm() {
   const film = films.find(film => film._id === filmId)
   return (
     <>
+  
       <Card border="light">
+      <Container>
+        <Row>
+      <div
+      className="bg-image d-flex justify-content-center align-items-center"
+      style={{
+        backgroundImage: `url("https://images.pexels.com/photos/3953965/pexels-photo-3953965.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")`,
+        margin: 40,
+        backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+           height:"80vh",
+           display: "flex",
+           flexDirection: "column",
+           justifyContent: "center",
+
+      }}
+      >
+        <Col>
         <Link to={`/film/${film._id}`}>
           <Card.Img variant="top" src={film.poster} height="220px" style={{ borderRadius: "10px" }} />
         </Link>
+        </Col>
+        <Col>
         <Card.Body>
           <Link to={`/film/${film._id}`} className="text-black" style={{ textDecoration: "none" }}>
             <Card.Title>{film.title}</Card.Title>
           </Link>
           <Card.Text className="text-muted">{film.description}</Card.Text>
         </Card.Body>
+        </Col>
+        </div>
+        </Row>
+        </Container>
       </Card>
+      
     </>
   )
 }
