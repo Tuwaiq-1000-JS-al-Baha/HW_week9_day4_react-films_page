e4b8eeb1e0777b751d09319b768543eb6a3545c6
AmjadMua <92171759+AmjadMua@users.noreply.github.com>
2021-12-09 07:27:06 +0300

    Done.

--- src/pages/OneFilm.js
+++ src/pages/OneFilm.js
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { Card } from "react-bootstrap"
+import { Card, Col, Row } from "react-bootstrap"
 import { useParams } from "react-router"
 import { Link } from "react-router-dom"
 import FilmsContext from "../utils/FilmsContext"
@@ -10,19 +10,39 @@ function OneFilm() {
   if (films.length === 0) return <h1>Loading...</h1>
 
   const film = films.find(film => film._id === filmId)
+
   return (
     <>
-      <Card border="light">
-        <Link to={`/film/${film._id}`}>
-          <Card.Img variant="top" src={film.poster} height="220px" style={{ borderRadius: "10px" }} />
-        </Link>
-        <Card.Body>
-          <Link to={`/film/${film._id}`} className="text-black" style={{ textDecoration: "none" }}>
-            <Card.Title>{film.title}</Card.Title>
-          </Link>
-          <Card.Text className="text-muted">{film.description}</Card.Text>
-        </Card.Body>
+      <Card
+        border="light"
+        className="d-flex align-items-center p-5 mt-5 "
+        style={{ backgroundColor: "rgba(141, 113, 79, 0.747)", width: "900px", marginLeft: "320px" }}
+      >
+        <Row>
+          <Col>
+            <img variant="top" src={film.poster} height="300px" style={{ borderRadius: "10px" }} />
+          </Col>
+          <Col>
+            <Card.Body>
+              <Card.Title>{film.title}</Card.Title>
+              <Card.Text className="text-muted">{film.description}</Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
       </Card>
+      <Row>
+        {film.actors.map(actor => (
+          <Card border="light" className="m-5">
+            <h2> Actors: </h2>
+            <Card.Img variant="top" src={actor.photo} height="220px" style={{ borderRadius: "10px", width: "200px" }} />
+            <Card.Body>
+              <Card.Title>
+                {actor.firstName} {actor.lastName}
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        ))}
+      </Row>
     </>
   )
 }
